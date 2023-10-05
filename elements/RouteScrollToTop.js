b8aa4b58776774f20e012f833faebc21bdbdf9de
husiantp <husain.fakih@teampumpkin.com>
2023-10-05 16:39:45 +0530

    routes addition

--- elements/RouteScrollToTop.js
+++ elements/RouteScrollToTop.js
@@ -1,8 +1,10 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import { usePathname } from 'next/navigation'
 
 const RouteScrollToTop = () => {
-  const { pathname } = useLocation();
+  // const { pathname } = useLocation();
+  const pathname = usePathname()
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import RouteScrollToTop from '@/elements/RouteScrollToTop'
+import ScrollToTop from 'react-scroll-to-top'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <RouteScrollToTop />
+      <Component {...pageProps} />
+      <ScrollToTop smooth color='#210D7D' />
+    </>
+  )
 }
