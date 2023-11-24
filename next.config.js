7773afc8d1878168a0065ec5df70ff51ec7c6e5e
husiantp <husain.fakih@teampumpkin.com>
2023-11-24 12:26:15 +0530

    public address change

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
     buildActivity: false,
   },
   env: {
-    PUBLIC_URL: 'https://ssrvmtemplate.ssrvmtrust.org.in',
+    // PUBLIC_URL: 'https://ssrvmtemplate.ssrvmtrust.org.in',
+    PUBLIC_URL: 'http://43.205.227.120:3000/',
     REACT_APP_MAIN_SSRVM_SITE_URL: 'https://ssrvmtemplate.ssrvmtrust.org.in',
     REACT_APP_LOCAL_SSRVM_SITE_URL: 'https://schoolapi.ssrvmtrust.org.in',
     SMTP_HOST: "smtp.gmail.com",
