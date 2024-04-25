79bb025820ad1403111534af3fad931510f04f3c
karmakar-subhajit <karmakar.subhajit2016@gmail.com>
2024-04-25 14:47:56 +0530

    ch nw

--- pages/api/contactMail.js
+++ pages/api/contactMail.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
 
     const host = req.headers.host;
 
-    console.log("hh", host)
+    console.log("hh", req.headers)
 
     const siteUrl = determineStrapiUrl(host);
 
