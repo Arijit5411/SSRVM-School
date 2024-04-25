2b7f1fd35438e6bbe912f10d8be15c704eeeb919
Harish Karumuthil <harish2704@gmail.com>
2024-04-26 01:42:28 +0530

    Revert "chaneg email to my id"
    This reverts commit f55b5691868d7b49b67623ade8edfa2236edc23f.

--- pages/api/contactMail.js
+++ pages/api/contactMail.js
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
     try {
         await transporter.sendMail({
             from: `<${process.env.SMTP_USER}>`,
-            to: `subhajit.karmakar@teampumpkin.com`,
+            to: mailList,
             // to: 'husain.fakih@teampumpkin.com',
             subject: `Contact Mail - ${full_name} - ${contact_no} `,
             text: `Contact Information`,
