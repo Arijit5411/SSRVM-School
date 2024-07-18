90d6476149b8bdc6cec940c823c5df608381daf2
runmusic581 <119038689+runmusic581@users.noreply.github.com>
2024-07-18 15:02:36 +0530

    hghkiiyi

--- components/Popups/ModalAward.js
+++ components/Popups/ModalAward.js
@@ -42,7 +42,7 @@ const ModalAward = ({ showModal, setShowModal, award }) => {
                 >
                     &times;
                 </button>
-                <div className='modal-cont-g3'>
+                <div className='modal-content-inner'>
                     <h2 className="font-bold mb-4">{award.award_name}</h2>
                     <img src={award.imageUrl} alt={award.award_name} className="award-image-custom" />
                     <p className="text-lg">{award.description}</p>
