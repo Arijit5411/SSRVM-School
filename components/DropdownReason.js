df2041cac49bb328142bc661a91a9e1617897178
runmusic581 <119038689+runmusic581@users.noreply.github.com>
2024-10-22 16:43:57 +0530

    kjbjjhvjhvd

--- components/DropdownReason.js
+++ components/DropdownReason.js
@@ -8,9 +8,9 @@ const {handleChange,selectedReasioError, selectedOption}=props
       <div className="input_contact_popup mt-2">
         <select value={selectedOption} onChange={handleChange}>
           <option value="">Select Reason</option>
-
           <option value="To meet Principal">To meet Principal</option>
           <option value="To meet class teacher">To meet class teacher</option>
+          <option value="To meet Administrator">To meet Administrator</option>
           <option value="Any other reason">Any other reason</option>
         </select>
       </div>
