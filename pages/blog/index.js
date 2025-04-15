9a45ff2ca5ab201fa459c6d91d8364bb7c65badb
runmusic581 <119038689+runmusic581@users.noreply.github.com>
2025-04-15 17:09:14 +0530

    blogggg

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,146 +1,161 @@
-import React, { Fragment, useState, useEffect } from "react";
+import { Fragment, useState } from "react";
+import Seo from "@/components/Seo";
+import Link from "next/link";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
-import Link from "next/link";
-const postsPerPage = 6;
 import { determineStrapiUrl } from "@/utils/strapiUtils";
-import Seo from "@/components/Seo";
 import Image from "next/image";
 
-export const getServerSideProps = async (context) => {
-  try {
-    const siteUrl = determineStrapiUrl(context);
+export async function getServerSideProps(context) {
+  const siteUrl = determineStrapiUrl(context);
 
-    const res = await fetch(`${siteUrl}/api/seo?populate=deep,10`);
+  const [categoriesRes, seoRes] = await Promise.all([
+    fetch(
+      `${siteUrl}/api/blog-categories?sort=id:desc&populate=deep,10`
+    ),
+    fetch(`${siteUrl}/api/seo?populate=deep,10`),
+  ]);
 
-    const res1 = await fetch(`${siteUrl}/api/blogs?sort=id:desc&populate=*`);
+  const data = await categoriesRes.json();
+  const SeoData = await seoRes.json();
 
-    const data = await res.json();
-    const data1 = await res1.json();
+  return {
+    props: {
+      blgCats: data?.data || [],
+      SeoData: SeoData?.data?.attributes?.Pages || null,
+      siteUrl,
+    },
+  };
+}
 
-    return {
-      props: {
-        seodata: data?.data?.attributes?.Pages ?? {},
-        blogProp: data1,
-        siteUrl,
-      },
-    };
-  } catch (error) {
-    console.error("Error fetching data:", error.message);
+function Blog({ blgCats, SeoData, siteUrl }) {
+  const [visibleBlogs, setVisibleBlogs] = useState(
+    blgCats.reduce((acc, category) => {
+      acc[category.id] = 3;
+      return acc;
+    }, {})
+  );
 
-    return {
-      props: {
-        data: [],
-      },
-    };
-  }
-};
-
-const Blog = ({ seodata, blogProp, siteUrl }) => {
-  console.log('seodata', seodata)
-  const [blog, setBlog] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
-
-  useEffect(() => {
-
-    if (blogProp && blogProp?.data) {
-      const sortedBlogs = blogProp.data.sort(
-        (a, b) => new Date(b.attributes.date) - new Date(a.attributes.date)
-      );
-      setBlog({ ...blogProp, data: sortedBlogs });
-    }
-  }, []);
-
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = blog?.data?.slice(indexOfFirstPost, indexOfLastPost);
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
-  //for arrow button in pagination
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      paginate(currentPage - 1);
-    }
+  const handleLoadMore = (categoryId, totalBlogs) => {
+    setVisibleBlogs((prev) => ({
+      ...prev,
+      [categoryId]: totalBlogs,
+    }));
   };
 
-  const handleNextPage = () => {
-    if (currentPage < Math.ceil(blog.data.length / postsPerPage)) {
-      paginate(currentPage + 1);
-    }
+  const handleShowLess = (categoryId) => {
+    setVisibleBlogs((prev) => ({
+      ...prev,
+      [categoryId]: 3,
+    }));
+  };
+
+  const renderBlogs = (blog, categoryId) => {
+    const visibleCount = visibleBlogs[categoryId] || 3;
+    const totalBlogs = blog.length;
+    const visibleItems = blog
+      .sort((a, b) => b.id - a.id)
+      .slice(0, visibleCount);
+
+    // const strapiUrl = process.env.NEXT_PUBLIC_STRAPI_API_URL || "";
+
+    return (
+      <>
+        <div className="row">
+          {visibleItems.map((item) => {
+
+            const imageUrl = item?.attributes?.image?.data?.attributes?.url
+              ? `${siteUrl}${item.attributes.image.data.attributes.url}`
+              : "/default.jpg";
+
+            return (
+              <div className="col-md-4 mb-4" key={item?.id}>
+                <Link href={`/blog/${item?.attributes?.slug}`} className="blog-item">
+                  <div className="image-wrap">
+                    <img
+
+                      src={imageUrl}
+                      alt={item?.attributes?.Title || "Blog Image"}
+                    />
+                  </div>
+                  <div className="content-wrap">
+                    <p>{item?.attributes?.Title}</p>
+                    <button className="text-muted-news">Read more</button>
+                  </div>
+                </Link>
+              </div>
+            );
+          })}
+
+        </div>
+        <div className="text-center mt-3">
+          {visibleCount < totalBlogs && (
+            <button
+              className="def-btn"
+              onClick={() => handleLoadMore(categoryId, totalBlogs)}
+            >
+              Load More
+            </button>
+          )}
+
+          {visibleCount > 3 && visibleCount >= totalBlogs && (
+            <button
+              className="def-btn"
+              onClick={() => handleShowLess(categoryId)}
+            >
+              Show Less
+            </button>
+          )}
+
+        </div>
+      </>
+    );
   };
 
   return (
     <>
-      <Seo SeoData={seodata} PageSlug={"blog"} />
+      <Seo SeoData={SeoData} PageSlug={"blog"} />
       <Fragment>
         <NavBar siteUrl={siteUrl} />
-        <div className="" style={{ padding: '160px 0 60px 0' }}>
-          <div className="container">
-            <h1 className="principal-mess">Blog</h1>
-          </div>
-          {blog ? (
-            <section className="container wrap-news-sec-2">
-              <div className="row">
-                {Array.isArray(currentPosts) && currentPosts.length > 0 ? (
-                  currentPosts.map((post) => (
-                    <div className="col-lg-4" key={post.id}>
-                      <div className="card wrap-news">
-                        <Image height={200} width={400}
-                          src={`${siteUrl}${post.attributes.image?.data?.attributes?.url}`}
-                          className="wrap-img-top"
-                          alt="..."
-                        />
-                        <div className="card-body">
-                          <p className="card-text-news">
-                            {post.attributes.date}
-                          </p>
-                          <p className="card-text-news">
-                            {post.attributes.Title}
-                          </p>
-                          <Link href={`/blog/${post.attributes.slug}`} className="text-muted-news">
-                            read more
-                          </Link>
-                        </div>
-                      </div>
+        <div className="" style={{ padding: "160px 0 60px 0" }}>
+          <section className="section section-p13s1 position-relative">
+            <div className="container position-relative zi-3">
+              <div className="section-title">
+                <h1 className="fs-50 fs-lg-80 fw-700 color-1 text-capitalize">
+                  Blog
+                </h1>
+              </div>
+
+              {blgCats.map((category) => {
+                const blogData =
+                  category?.attributes?.blog?.data ||
+                  category?.attributes?.blogs?.data;
+
+                return (
+                  <div className="blogs-wrap mt-5 mt-lg-5" key={category.id}>
+                    <div className="sub-title">
+                      <h4 className="fs-30 fs-md-34 fs-lg-38 fw-700 color-1">
+                        {category?.attributes?.Title}
+                      </h4>
                     </div>
-                  ))
-                ) : (
-                  <p>No blog posts available.</p>
-                )}
-              </div>
-              <div className="pagination-blog">
-                {currentPage > 1 && (
-                  <button onClick={handlePrevPage}>&larr; Prev</button>
-                )}
 
-                {blog?.data?.length > postsPerPage &&
-                  Array.from(
-                    { length: Math.ceil(blog.data.length / postsPerPage) },
-                    (_, index) => (
-                      <button
-                        key={index}
-                        onClick={() => paginate(index + 1)}
-                        className={currentPage === index + 1 ? "active" : ""}
-                      >
-                        {index + 1}
-                      </button>
-                    )
-                  )}
-
-                {currentPage < Math.ceil(blog.data.length / postsPerPage) && (
-                  <button onClick={handleNextPage}>Next &rarr;</button>
-                )}
-              </div>
-            </section>
-          ) : (
-            <p>Loading blog posts...</p>
-          )}
+                    {Array.isArray(blogData) && blogData.length > 0 ? (
+                      <div className="p13s1-slider-f1 norm-3">
+                        {renderBlogs(blogData, category.id)}
+                      </div>
+                    ) : (
+                      <p className="text-muted">No blogs available in this category.</p>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          </section>
         </div>
         <Footer siteUrl={siteUrl} />
       </Fragment>
     </>
   );
-};
+}
 
 export default Blog;
